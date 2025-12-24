9087c46acf79094ff097feee4c03ad93be985e9d
Rahul Kesharwani <132988447+iamrkesharwani@users.noreply.github.com>
2025-12-24 16:31:17 +0530

    small projects added

---
+++ use-reducer/small-projects/src/App.jsx
@@ -0,0 +1,17 @@
+import Counter from './Counter/Counter';
+import Form from './Form/Form';
+import Fetch from './Fetch/Fetch';
+import Todo from './Todo/Todo';
+
+const App = () => {
+  return (
+    <>
+      {/* <Counter /> */}
+      {/* <Form /> */}
+      {/* <Fetch /> */}
+      {/* <Todo /> */}
+    </>
+  );
+};
+
+export default App;
